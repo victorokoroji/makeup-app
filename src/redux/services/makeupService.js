23aab7516761c6dd7e8e888bf5c3ac1b0d4a1323
unknown <okorojiebube2@gmail.com>
2022-04-22 05:30:45 -0600

    Setup redux

---
+++ src/redux/services/makeupService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json';
+
+const getMoviesFromServer = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}`);
+    const datas = response.data;
+    return datas;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default getMoviesFromServer;
