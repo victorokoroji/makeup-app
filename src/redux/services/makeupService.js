7bece4bdac8c54dd9076d7b20c826b43905facc8
Okoroji Victor Ebube <okorojiebube2@gmail.com>
2022-04-23 13:25:46 +0100

    Get makeup by foundation product type

--- src/redux/services/makeupService.js
+++ src/redux/services/makeupService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://makeup-api.herokuapp.com/api/v1/products.json';
+const BASE_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=foundation';
 
 const getMoviesFromServer = async () => {
   try {
