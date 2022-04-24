140cd92ce033fd78edb20b751b4355bfdd549024
unknown <okorojiebube2@gmail.com>
2022-04-24 00:48:55 -0600

    Fix linter errror

--- src/__tests__/makeups.test.js
+++ src/__tests__/makeups.test.js
@@ -1,6 +1,6 @@
-import makeupReducer, { getAllMakeUps } from '../redux/makeups/makeups';
+import makeupReducer, { getAllFoundations } from '../redux/makeups/makeups';
 
 it('reducer should return list of makeups', () => {
   const previousState = [];
-  expect(makeupReducer(previousState, getAllMakeUps())).toBeDefined();
+  expect(makeupReducer(previousState, getAllFoundations())).toBeDefined();
 });
